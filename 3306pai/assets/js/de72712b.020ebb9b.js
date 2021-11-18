a96ecbfad9d5b0349e76a3ce83fa502a78469bc4
Chasen-Zhang <787025321@qq.com>
2021-11-18 18:23:20 +0800

    test

---
+++ 3306pai/assets/js/de72712b.020ebb9b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdatabend_official_front=self.webpackChunkdatabend_official_front||[]).push([[584],{4129:function(e){e.exports=JSON.parse('{"permalink":"/3306pai/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ 3306pai/json/company.js
@@ -0,0 +1,50 @@
+const mentors = {
+  comment: {
+    _comment: "这是赞助商和主办方的数据",
+    title: "公司名称",
+    logo: "公司log 地址对应static文件夹下，注意：static不用添加,static/img/home/greatDB.png =》img/home/greatDB.png，参考下方示例",
+    link: "公司链接",
+    sponsorData: "赞助商数据",
+    hostData: "主办方数据"
+  },
+  sponsorData: [
+    {
+      title: "万里云数据库",
+      logo: "img/home/greatDB.png",
+      link: 'https://www.greatdb.com/'
+    },
+    {
+      title: "爱可生",
+      logo: "img/home/action.png",
+      link: 'https://www.actionsky.com/'
+    },
+    {
+      title: "华为",
+      logo: "img/home/huawei.png",
+      link: 'https://www.huaweicloud.com/'
+    },
+    {
+      title: "Databend",
+      logo: "img/home/databend.png",
+      link: 'https://databend.com/'
+    },
+    {
+      title: "RadonDB",
+      logo: "img/home/radonDB.png",
+      link: 'https://radondb.io/'
+    },
+  ],
+  hostData: [
+    {
+      title: "3306π",
+      logo: "img/home/3306π.png",
+      link: null
+    },
+    {
+      title: "Redis中国用户组",
+      logo: "img/home/redis.png",
+      link: null
+    },
+  ]
+}
+export default mentors;
