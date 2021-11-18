8ed4848659aff60f4a5134984442238e41fc6555
Chasen-Zhang <787025321@qq.com>
2021-11-18 19:29:38 +0800

    test ci

--- src/pages/sponser/index.js
+++ src/pages/sponser/index.js
@@ -6,7 +6,7 @@ export default function Company() {
       title={`3306π - 3306π 围绕 MySQL，大数据，云原生等核心技术，交流前沿数据库相关技术，分享干货知识，拒绝放水。 `}
       description="我们也在全国招聘志愿者： 地区负责人， 技术 writer, 如果你也想参与到 3306π 活动中，请联系我们。">
       <main>
-        to bo continue ...
+        to bo continue ...  TEST
       </main>
     </Layout>
   )
